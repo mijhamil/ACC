12f6d5e105ddaf6105c1ca4d477784d2d9131edc
mijhamil <mijhamil@ucsc.edu>
2019-01-28 18:12:45 -0800

    Comments and formatting

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "490cfab00afecf1909db";
+/******/ 	var hotCurrentHash = "1d93370a5f083c6fe8cb";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
